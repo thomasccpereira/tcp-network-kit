5711e6be864928b38052a982edbcfe784785e72c
Thomás Pereira <thomascalmon.cp@gmail.com>
2026-05-08 13:50:22 -0300

    increased version of CoreResources

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
          targets: ["NetworkKit"]),
    ],
    dependencies: [
-      .package(url: "https://github.com/thomasccpereira/tcp-core-resources", from: "1.0.0"),
+      .package(url: "https://github.com/thomasccpereira/tcp-core-resources", from: "1.0.1"),
    ],
    targets: [
       .target(
